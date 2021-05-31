de27271c8597616d1f2c18bde5b2d1ec61a714b7
Ricardo Martínez <74849073+Rockolino99@users.noreply.github.com>
2021-05-31 11:28:46 -0500

    Mejora seguridad

--- application/views/view_seguridad.php
+++ application/views/view_seguridad.php
@@ -15,10 +15,50 @@
             <input type="text" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1">
         </div>
         <div class="input-group mb-3 col-2 mx-auto">
-            <span class="p-2" style="background-color: red; color: white; text-align: center;">Cajero</span>
+            <span class="badge badge-danger" style="align-self: center;">Cajero</span>
         </div>
         <div class="input-group mb-3 col-2">
             <button class="btn btn-success"><i class="fas fa-save"></i></button>
         </div>
     </div>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Nombre de usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon2"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Nombre de usuario" aria-label="Nombre de usuario" aria-describedby="basic-addon2">
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-lock"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1">
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group" style="height: 100%;">
+                        <span class="badge badge-danger"  style="height: 100%;">Cajero</span>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group mx-auto">
+                        <button class="btn btn-success"><i class="fas fa-save"></i></button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
