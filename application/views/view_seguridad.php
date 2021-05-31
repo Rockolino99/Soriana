83e556da348095667aa14b292ce2464c19e2a4ce
Ricardo Martínez <74849073+Rockolino99@users.noreply.github.com>
2021-05-31 09:20:32 -0500

    Seguridad
    Creación de apartado de seguridad

---
+++ application/views/view_seguridad.php
@@ -0,0 +1,24 @@
+<h2 class="text-center">SEGURIDAD</h2>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="input-group mb-3 col-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon2"><i class="fas fa-user"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Nombre de usuario" aria-label="Nombre de usuario" aria-describedby="basic-addon2">
+        </div>
+        <div class="input-group mb-3 col-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-lock"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3 col-2 mx-auto">
+            <span class="p-2" style="background-color: red; color: white; text-align: center;">Cajero</span>
+        </div>
+        <div class="input-group mb-3 col-2">
+            <button class="btn btn-success"><i class="fas fa-save"></i></button>
+        </div>
+    </div>
+</div>
