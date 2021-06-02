b9928468bc0a851c0140c7e3e6d19307b6fe2d2a
Ricardo Martínez <rickymuro9@gmail.com>
2021-06-01 19:14:39 -0500

    Vista de administrador
    En desarrollo

---
+++ application/views/view_administracion.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="application/assets/css/administracion.css">
+
+<div class="wrap">
+
+    <ul class="tabs group">
+        <li><a class="active" href="#one">Light &</a></li>
+        <li><a href="#two">Sexy</a></li>
+        <li><a href="#three">Tabs</a></li>
+    </ul>
+
+    <div id="content">
+        <section id="one">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </section>
+        <section id="two" style="display: none;">Sexy sexy consectetur adipisicing elit. Pariatur modi quod quo iure recusandae eligendi q.t, consectetur adipisicing elit. Pariatur modi quod quo iureq</section>
+        <section id="three" style="display: none;">Tabs , consectetur adipisicing elit. Pariatur modi quod quo iure recusandae eligendi q.t, consectetur adipisicing elit. Pariatur modi quod quo iureq
+            Tabs , consectetur adipisicing elit. Pariatur modi quod quo iure recusandae eligendi q.t, consectetur adipisicing elit. Pariatur modi quod quo iureq
+            Tabs , consectetur adipisicing elit. Pariatur modi quod quo iure recusandae eligendi q.t, consectetur adipisicing elit. Pariatur modi quod quo iureq</section>
+    </div>
+
+</div>
+
+<script src="application/assets/js/administracion.js"></script>
