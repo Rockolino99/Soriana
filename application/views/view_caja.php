302e7079f659aaca7b678b24bce276726bb55cb0
Ricardo Martínez <rickymuro9@gmail.com>
2021-06-06 21:27:39 -0500

    Vista de cajas
    Mejora

--- application/views/view_caja.php
+++ application/views/view_caja.php
@@ -1,25 +1,27 @@
 <link rel="stylesheet" href="application/assets/css/caja.css">
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         <div class="wrap col-8">
             <h3 class="text-center mt-3">PRODUCTOS</h3>
             <br>
             <div class="content">
-                <section id="one">
-                    <table id="table_listaProductos" class="display">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Cantidad</th>
-                                <th>Precio</th>
-                                <th>Proveedor</th>
-                                <th style="width: 90px;">Piezas</th>
-                                <th>Opciones</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </section>
+                <div class="card">
+                    <section id="one">
+                        <table id="table_listaProductos" class="display">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nombre</th>
+                                    <th>Cantidad</th>
+                                    <th>Precio</th>
+                                    <th>Proveedor</th>
+                                    <th style="width: 90px;">Piezas</th>
+                                    <th>Opciones</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </section>
+                </div>
             </div>
         </div>
         <div class="wrap col-4">
